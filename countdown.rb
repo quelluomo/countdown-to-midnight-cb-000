7b6b9f9e0b52c470053910852d3c7e68f512e81d
Matteo Muscas <thatman.j@gmail.com>
2018-11-13 11:49:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,8 @@
 #write your code here
 
 def countdown(n)
-  n = 10
   while n > 0
-    puts '#{n} SECOND(S)!'
-    require.pry
+    puts "#{n} SECOND(S)!'"
     n -= 1
   end
   return "HAPPY NEW YEAR!"
